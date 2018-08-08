90db0f986186f3c767cb8e85b9bce986aecb0663
Rodrigo Rocha <rodrigorgs@gmail.com>
2018-08-07 23:07:15 -0300

    Manipula migrations pra evitar problemas com PostgreSQL.
    Estava dando problema ao mudar o tipo da coluna posição,
    de string para integer.

--- db/migrate/20180806232853_create_vagas.rb
+++ db/migrate/20180806232853_create_vagas.rb
@@ -2,7 +2,7 @@ class CreateVagas < ActiveRecord::Migration[5.2]
   def change
     create_table :vagas do |t|
       t.references :aluno, foreign_key: true
-      t.string :posicao
+      t.integer :posicao
       t.datetime :data_chamada
 
       t.timestamps
--- db/migrate/20180807022948_change_posicao_to_integer.rb
+++
@@ -1,5 +0,0 @@
-class ChangePosicaoToInteger < ActiveRecord::Migration[5.2]
-  def change
-    change_column :vagas, :posicao, :integer
-  end
-end
